e854e02a0e32c417d64f372494a088833d745c2b
gjspencer22 <gjspencer22@gmail.com>
2021-08-05 18:08:29 -0600

    added timer function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById 
-('question-container')
+const questionContainerElement = document.getElementById
+    ('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById
-('answer-buttons')
+    ('answer-buttons')
 
 let shuffledQuestions, currentQuestionIndex
 
@@ -13,32 +13,40 @@ nextButton.addEventListener('click', () => {
     currentQuestionIndex++
     setNextQuestion()
 })
-let secondsLeft = 76;
 
-let timer = document.getElementById("timer");
 
-let scoresDiv = document.getElementById("scores-div");
+// let scoresDiv = document.getElementById("scores-div");
 
-function setTime() {
-    displayQuestions();
-    let timerInterval = setInterval(function() {
-      secondsLeft--;
-      timer.textContent = "";
-      timer.textContent = "Time: " + secondsLeft;
-      if (secondsLeft <= 0 || questionCount === questions.length) {
-        clearInterval(timerInterval);
-        captureUserScore();
-      } 
-    }, 1000);
-  }
-  
+// function setTime() {
+//     displayQuestions();
+//     let timerInterval = setInterval(function () {
+//         secondsLeft--;
+//         timer.textContent = "";
+//         timer.textContent = "Time: " + secondsLeft;
+//         if (secondsLeft <= 0 || questionCount === questions.length) {
+//             clearInterval(timerInterval);
+//             captureUserScore();
+//         }
+//     }, 1000);
+// }
+
+
+function correct() {
+    document.getElementById("answer").textContent ="Correct!"
+}
+function incorrect() {
+    document.getElementById("answer").textContent ="Incorrect! :("
+    count = count -1;
+}
+
 
 function startGame() {
-startButton.classList.add('hide')
-shuffledQuestions = questions. sort(() => Math.random() -.5)
-currentQuestionIndex = 0
-questionContainerElement.classList.remove('hide')
-setNextQuestion()
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+    // setInterval();
 }
 
 function setNextQuestion() {
@@ -49,7 +57,7 @@ function setNextQuestion() {
 function showQuestion(question) {
     questionElement.innerText = question.question
     question.answers.forEach(answer => {
-        const button = document.createElement ('button')
+        const button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
         if (answer.correct) {
@@ -60,24 +68,52 @@ function showQuestion(question) {
     })
 
 }
+var count = 100;
+var btn = document.getElementById('start-btn');
+
+
+    btn.addEventListener("click", function () {
+        document.getElementById('count').innerHTML = "Time left:" + count;
+    count--;
+    if (count <= 0) {
+        clearInterval(interval);
+        document.getElementById('count').innerHTML = 'Out of time!';
+        alert("You're out of time!");
+     }
+    }, 1000);
+
+
+// var interval = setInterval(function () {
+    
+//     document.getElementById('count').innerHTML = "Time left:" + count;
+//     count--;
+//     if (count <= 0) {
+//         clearInterval(interval);
+//         document.getElementById('count').innerHTML = 'Out of time!';
+//         alert("You're out of time!");
+//     }
+// }, 1000);
+
+
+
 
 function resetState() {
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
         answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+            (answerButtonsElement.firstChild)
     }
 }
 function selectAnswer(e) {
     const selectedButton = e.target
     const correct = selectedButton.dataset.correct
-    setStatusClass (document.body, correct)
+    setStatusClass(document.body, correct)
     Array.from(answerButtonsElement.children).forEach(button => {
-        setStatusClass(button,button.dataset.correct)
+        setStatusClass(button, button.dataset.correct)
     })
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide')
-    } else{
+    } else {
         startButton.innerText = 'Restart'
         startButton.classList.remove('hide')
     }
@@ -87,46 +123,57 @@ function selectAnswer(e) {
 
 function setStatusClass(element, correct) {
     clearStatusClass(element)
-    if(correct) {
+    if (correct) {
         element.classList.add('correct')
-    } 
-    else{
-        element.classList.add('wrong')
+        correct();
     }
-function clearStatusClass(element) {
-    element.classList.remove('correct')
-    element.classList.remove('wrong')
-}
+    else {
+        element.classList.add('wrong')
+        incorrect();
+    }
+    function clearStatusClass(element) {
+        element.classList.remove('correct')
+        element.classList.remove('wrong')
+    }
 
 }
 const questions = [
     {
         question: 'Arrays in Javascript can be used to store what?',
         answers: [
-            {text: 'Booleans', correct: false},
-            {text: 'Numbers and Strings', correct: false},
-            {text: 'Arrays', correct: false},
-            {text: 'All of the Above', correct: true}
-            
+            { text: 'Booleans', correct: false },
+            { text: 'Numbers and Strings', correct: false },
+            { text: 'Arrays', correct: false },
+            { text: 'All of the Above', correct: true }
+
         ]
     },
     {
         question: 'HTML is a graphical design tool used to develop web pages that are displayed with a browser.',
         answers: [
-        
-            {text: 'True', correct: false},
-            {text: 'False', correct: true}
-            
+
+            { text: 'True', correct: false },
+            { text: 'False', correct: true }
+
         ]
     },
 
     {
         question: 'What is 3+2?',
         answers: [
-            {text: '5', correct: true},
-            {text: '22', correct: false}
+            { text: '5', correct: true },
+            { text: '22', correct: false }
         ]
     },
-        
+
 
 ]
+const lastQuestion = questions.length - 1;
+
+score = count; 
+console.log(score);
+
+
+// function endGame{
+
+// }
